a7d70172cd0518efeab4b3a68e1bb060bf731d90
Garrad <m.garrad@bristol.ac.uk>
2025-07-22 22:01:42 +0100

    Added list materials

--- Week_1_variables_operators_and_algorithms/Lab_1_variables_and_operators/Exercises/Ex1.py
+++ Week_1_variables_operators_and_algorithms/Lab_1_variables_and_operators/Exercises/Ex1.py
@@ -1,5 +1,1 @@
 # Exercise 1: Variables and Operators - Running code in a terminal and via an IDE
-
-radius = 2
-area = 3.14 * radius ** 2
-print("The area of a circle with radius", radius, "is", area)
