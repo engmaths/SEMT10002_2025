58a68b051c8293c1dda512e287a4ca64f92bbfd1
Arthur Richards <aeagr@bristol.ac.uk>
2025-09-27 00:33:06 +0100

    Week 2 lab now with sample answers and all files needed

---
+++ Week_2_if_then_else/Lab_2_if_then_else/Exercises/Ex1.py
@@ -0,0 +1,10 @@
+'''
+## Exercise 1 - Positive and negative numbers
+
+Write some code that can determine whether a number (provided to you as a variable `number') is positive, negative, or zero.
+'''
+
+# replace '???' with trial values
+number = '???'
+
+# your code here
---
+++ Week_2_if_then_else/Sample Answers/Ex1.py
@@ -0,0 +1,14 @@
+'''
+## Exercise 1 - Positive and negative numbers
+
+Write some code that can determine whether a number (provided to you as a variable `number') is positive, negative, or zero.
+'''
+
+number = -12
+
+if number > 0:
+    print('Number',number,'is positive')
+elif number < 0:
+    print('Number',number,'is negative')
+if number == 0:
+    print('Number',number,'is zero')
