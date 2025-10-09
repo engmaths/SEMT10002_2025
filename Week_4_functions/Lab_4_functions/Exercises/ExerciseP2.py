3ab9cec6953184f952b968c86d13e4f200223407
Arthur Richards <aeagr@bristol.ac.uk>
2025-10-09 22:11:32 +0100

    Added the library scaffolding for P2

---
+++ Week_4_functions/Lab_4_functions/Exercises/ExerciseP2.py
@@ -0,0 +1,41 @@
+# robot simulation
+
+'''
+## Task
+
+Write a program that simulates a robot driving the lawnmower search pattern from Week 3.  Your robot should stop when it gets within 100mm distance of the point (1200,-400), representing collision with an obstacle.
+
+Use functions where appropriate to make your code managable and clear.
+
+> A good function does one job, well.
+
+You have all the pieces to do this already, stretching back to Week 1.  The task here is to assemble and test them.  A tidy structure with functions will make it easy to test and degug as you go.
+
+Make your file a library called `ExerciseP2.py`, including a function called `main` that runs the simulation described.  Use the `if __name__` trick so that the simulation also runs if you invoke your file as a script.  _The template will help you with this._
+
+> Use the script `test_P2.py` to test your library: running `test_p2.py` should run your simulation.
+'''
+
+# import the trigonometry
+from math import cos, sin
+
+# robot constants
+robot_name = "Daneel"
+robot_radius = 160 # mm
+# put any other constants you need here
+
+# utility functions...
+def func():
+    # your code here
+    '???' # <-- replace
+
+# any more functions you need in here
+
+# main simulation function
+def main():
+    # your code here
+    '???' # <-- replace
+
+# magic to run the main function if invoked as a script
+if __name__=='__main__':
+    main()
