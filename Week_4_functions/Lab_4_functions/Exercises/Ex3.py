d4ea87054d5c746800f9ff9d930c34e15a775dc3
Garrad <m.garrad@bristol.ac.uk>
2025-10-14 11:01:32 +0100

    Added comprehension checks to functions worksheet

---
+++ Week_4_functions/Lab_4_functions/Exercises/Ex3.py
@@ -0,0 +1,11 @@
+'''
+Find the solution of the equation below numerically.
+
+$ 10e^{\frac{x}{10}} + x - 11 = 0$
+
+Suggested approach:
+ - Implement the expression above as a function
+ - You'll need `from math import exp` before you can use the exponential function
+ - Write a function to find the lowest integer $x$ such that the solution is in the interval $x$ and $x+1$
+ - Write a function that reduces the interval further using a bisection search, i.e. halving it at every step
+ '''
