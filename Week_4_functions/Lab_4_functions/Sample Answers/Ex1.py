247894b05cc0364c375c7643ceaa25b080b5f3dc
Arthur Richards <aeagr@it103246.wks.bris.ac.uk>
2025-10-14 13:47:45 +0100

    Started answers to Lab 4.  3 only part done and P2 not started

---
+++ Week_4_functions/Lab_4_functions/Sample Answers/Ex1.py
@@ -0,0 +1,10 @@
+'''
+Here's a function I have written to take the average of two numbers.  
+Use the function to find the average of 19,315 and 22,664.
+'''
+
+def average(m,n):
+    return 0.5*(m+n)
+
+a = average(19315,22664)
+print('Average is',a)
