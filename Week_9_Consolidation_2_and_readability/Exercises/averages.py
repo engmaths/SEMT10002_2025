20570a8c6833dd6de4e2c62568d1dfa2dedceedd
Garrad <m.garrad@bristol.ac.uk>
2025-11-07 16:20:24 +0000

    Added week 9 cconsolidation stuff

---
+++ Week_9_Consolidation_2_and_readability/Exercises/averages.py
@@ -0,0 +1,29 @@
+import sys 
+
+def calculate_mean(numbers):
+
+    return 0.0
+
+def calculate_median(numbers):
+
+    return 0.0
+
+def calculate_mode(numbers):
+
+    return 0.0
+
+def main(args):
+
+    return
+
+if __name__ == '__main__':
+    
+    #This line will run the main function of your code- uncomment this when you're ready to run everything.
+	#main(sys.argv[1:])
+     
+    #The code below will run the functions for calculating each statistic
+    #once these are done, you should comment these out and use the main function instead
+    test_numbers = [1, 2, 3, 4, 5]
+    print("Mean: ", calculate_mean(test_numbers))
+    print("Median: ", calculate_median(test_numbers))
+    print("Mode: ", calculate_mode(test_numbers))
