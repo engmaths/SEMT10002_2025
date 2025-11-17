0519a97e90608f32c7b569aedd146523ec62926e
hphilamore <hemma.philamore@bristol.ac.uk>
2025-11-17 17:04:54 +0000

    HP updated sample answer P3

---
+++ Week_10_Dictionaries_and_Tuples/Lab_7_dictionaries_and_tuples/Exercises/Ex1.py
@@ -0,0 +1,18 @@
+'''
+================
+Exercise 1
+================
+
+The dictionary shows currency exchange rates
+
+Modify the code:
+- Look up the current rate for converting USD to EUR. Update the dictionary to reflect new data.
+- Look up the current rate for converting USD to JPY. Update the dictionary to include the new entry.
+- Try to use a list instead of a tuple as the key and observe what happens
+'''
+
+exchange_rates = {
+    ("USD", "EUR"): 0.93,
+    ("EUR", "GBP"): 0.86,
+    ("USD", "JPY"): 151.7
+}
