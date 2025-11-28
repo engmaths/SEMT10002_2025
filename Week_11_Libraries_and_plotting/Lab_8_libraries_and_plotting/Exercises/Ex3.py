2cc641ad5b388339b08d836ac557ad26916e00ea
hphilamore <hp12384@bristol.ac.uk>
2025-11-28 14:07:37 +0000

    HP added week 11 exercsies

--- Week_11_Libraries_and_plotting/Lab_8_libraries_and_plotting/Exercises/Ex3.py
+++ Week_11_Libraries_and_plotting/Lab_8_libraries_and_plotting/Exercises/Ex3.py
@@ -3,10 +3,10 @@
 Exercise 3
 ================ 
 
-1. Calculate the second moment of area, $I$ ($\mathrm{m}^4$) of each beam.
+1. Calculate the second moment of area of each beam.
 Convert the imported data to different units if needed.  
 
-2. Calculate the natural frequency, $f_n$ ($\mathrm{Hz}$) of each beam:
+2. Calculate the natural frequency of each beam:
 
 3. Generate a scatter plot showing natural frequency against the beam density. 
 
@@ -15,6 +15,7 @@ Sort the data points into the four types of wood, based on density.
 You will need to manually set the threshold values to sort the data 
 (Later in the unit we will study algorithms to automate this process of clustering similar data points)
 Re-plot the data so that each type of wood is plotted in a different colour. 
+Save the plot as a .png file
 '''
 
 
