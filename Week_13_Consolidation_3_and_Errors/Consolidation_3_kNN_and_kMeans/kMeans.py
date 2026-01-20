e905db522d73693bafab4a9bd235186e630be4a3
Garrad <m.garrad@bristol.ac.uk>
2026-01-20 13:26:01 +0000

    Final updates to week 13

--- Week_13_Consolidation_3_and_Errors/Consolidation_3_kNN_and_kMeans/kMeans.py
+++ Week_13_Consolidation_3_and_Errors/Consolidation_3_kNN_and_kMeans/kMeans.py
@@ -4,7 +4,7 @@ file = "heart_data.csv"
 
 #Your code for reading and visualising the data goes here.
 
-def initialise_clusters(k, data):
+def initialise_means(k, data):
 
     #means should be a list of k points, where each point is an tuple (feature1, feature2).
     initial_means = []
@@ -15,7 +15,7 @@ def assign_data_to_clusters(centroids, data):
     #Your code goes here
 
     #Clusters should be a list of tuples. Each list should contain all data points assigned to a cluster
-    clusters = [(,) for val in range(len(centroids))]
+    clusters = [[] for val in range(len(centroids))]
 
     return clusters
 
@@ -34,14 +34,14 @@ def plot_clusters(clusters):
 def kMeans(k, data, max_iterations = 100):
 
     #We need to randomly initialise the means (or centroids) before we can start
-    means = initalise_means(k, data)
+    means = initialise_means(k, data)
 
     #kMeans doesn't always converge, so we set a maximum number of iterations to stop our code from running forever. 
     for iteration in range(max_iterations):
         #We start by assigning data to clusters
-        clusters = assign_data_to_clusters(centroids, data)
+        clusters = assign_data_to_clusters(means, data)
         #Next we update our centroids
-        centroids = calculate_cluster_means()
+        means = calculate_cluster_means(clusters)
         #Sometime it's helpful to visualise each step of the algorithm- uncomment the line below to see that.
         #plot_clusters(clusters)
 
